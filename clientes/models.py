ff8f5291d7c17cafa673c23e32a23d81973a4fb1
JorgeCespedesCA <jorgecespedesand1@gmail.com>
2025-11-05 22:53:36 -0400

    Agrega modelo Cliente (nombre, apellido, ci_nit) y migración inicial

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    ci_nit = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
