922d2c7895a6e7ce33413ddf7f90ca23aa7ef894
aaron <www.agentepotasio@gmail.com>
2025-11-12 09:42:58 -0400

    Fix: include ventas urls and remove trailing garbage

--- backend/urls.py
+++ backend/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# ...existing code...
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings # <-- NUEVO
@@ -24,9 +9,11 @@ urlpatterns = [
     path('reservas/', include('reservas.urls')),
     # path('auth/', include('usuarios.urls')), # (Descomenta cuando tu compañero termine)
     path('pedidos/', include('pedidos.urls')), # <-- Registramos la nueva app
+    path('ventas/', include('ventas.urls')),   # <-- Añadido para reporte_ventas
     path('', include('core.urls')),
 ]
 
 # Esto permite ver las imágenes subidas mientras estamos en modo DEBUG (desarrollo)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ...existing code...
